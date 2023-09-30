b963c89d9e974982f25ce958fe37c3a0e7fed706
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-09-30 13:38:09 +0900

    feat: chat 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ResultsModule } from './results/results.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [PrismaModule, ResultsModule],
+  imports: [PrismaModule, ResultsModule, ChatModule],
 })
 export class AppModule {}
