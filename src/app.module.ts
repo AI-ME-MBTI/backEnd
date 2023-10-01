41afaec2b2477abe1517ad14dfa99741ed4f90af
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-10-01 17:10:18 +0900

    feat: user 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ResultsModule } from './results/results.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, ResultsModule],
+  imports: [PrismaModule, ResultsModule, UsersModule],
 })
 export class AppModule {}
