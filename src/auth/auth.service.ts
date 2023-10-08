7a4c698a4d494f63c6b47cd3cba0a815326866ee
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-10-09 01:35:30 +0900

    feat: 토큰 발급 API 생성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async getToken() {
+    const user = await this.usersService.create();
+
+    const payload = {
+      userId: user.id,
+    };
+
+    return {
+      statusCode: HttpStatus.CREATED,
+      data: {
+        message: ['사용자의 토큰이 정상적으로 생성되었습니다.'],
+        user: {
+          id: user.id,
+          accessToken: await this.jwtService.signAsync(payload),
+          createdAt: user.createdAt,
+        },
+      },
+    };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { v1 as uuid } from 'uuid';
 
@@ -10,21 +10,11 @@ export class UsersService {
     const user = await this.prismaService.users.create({
       data: {
         id: uuid(),
-        mbti: null,
         createdAt: new Date(),
       },
     });
 
-    return {
-      statusCode: HttpStatus.CREATED,
-      data: {
-        message: ['사용자가 정상적으로 생성되었습니다.'],
-        user: {
-          id: user.id,
-          createdAt: user.createdAt,
-        },
-      },
-    };
+    return user;
   }
 
   async findUser(userId: string) {
